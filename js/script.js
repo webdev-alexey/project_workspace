84cb638430ef472dc34286d405ec06090c4425b4
webdev-alexey <alexey.anasov@yandex.by>
2023-09-20 17:48:21 +0300

    передал в select города из API

--- js/script.js
+++ js/script.js
@@ -1,6 +1,40 @@
-const citySelect = document.querySelector("#city");
+const API_URL = "https://workspace-methed.vercel.app/";
+const LOCATION_URL = "api/locations";
 
-const cityChoices = new Choices(citySelect, {
-    searchEnabled: false,
-    itemSelectText: "",
-})
+const getData = async (url, cbSuccess, cbError) => {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        cbSuccess(data);
+    } catch (err) {
+        cbError(err);
+    }
+};
+
+const init = () => {
+    const citySelect = document.querySelector("#city");
+
+    const cityChoices = new Choices(citySelect, {
+        itemSelectText: "",
+    });
+
+    getData(
+        `${API_URL}${LOCATION_URL}`, 
+        (locationData) => {
+            const locations = locationData.map(location => ({
+                value: location,
+            }));
+            cityChoices.setChoices(
+                locations,
+                "value",
+                "label",
+                true,
+            );
+        },
+        (err) => {
+            console.log(err);
+        }
+    );
+};
+
+init();
